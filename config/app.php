60eb44c57e219fe3504462e5ac2ad0f3f9a953d6
Jhourlad Estrella <jhourlad01@gmail.com>
2016-08-19 18:27:14 +0800

    Facebook library anf config

--- config/app.php
+++ config/app.php
@@ -154,6 +154,8 @@ return [
          * Package Service Providers...
          */
 
+        Laravel\Socialite\SocialiteServiceProvider::class,
+
 
         /*
          * Application Service Providers...
@@ -162,6 +164,8 @@ return [
         App\Providers\AuthServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+
+
 
     ],
 
@@ -209,6 +213,7 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
 
+        'Socialite' => Laravel\Socialite\Facades\Socialite::class,
     ],
 
 ];
--- config/services.php
+++ config/services.php
@@ -39,4 +39,9 @@ return [
         'secret' => env('STRIPE_SECRET'),
     ],
 
+    'facebook' => [
+        'client_id' => '690344774435367',
+        'client_secret' => 'ebc50d3fd1d2f7286e02d247e5751ef4',
+        'redirect' => '',
+    ],
 ];
