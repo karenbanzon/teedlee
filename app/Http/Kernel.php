81330221c7e0ca9cef4e942e8a629095a11cefd9
Jhourlad Estrella <jhourlad01@gmail.com>
2016-08-27 00:18:44 +0800

    Auth and HTTPS security

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -34,6 +34,10 @@ class Kernel extends HttpKernel
         'api' => [
             'throttle:60,1',
         ],
+
+        'https' => [
+            \Teedlee\Http\Middleware\HttpsRequire::class,
+        ],
     ];
 
     /**
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -12,16 +12,23 @@
 */
 
 Route::get('/', 'HomeController@index');
-Route::post('auth', 'AuthController@index');
-Route::get('login', 'AuthController@login');
-Route::get('signup', 'AuthController@signup');
-Route::get('oauth/{service}', 'AuthController@oauth');
-Route::get('oauth/callback/{service}', 'AuthController@oauthCallback');
-Route::get('logout', 'AuthController@logout');
-Route::get('user', 'UserController@index');
-Route::post('user/create', 'UserController@create');
-Route::get('user/activate/{user}/{token}', 'UserController@activate');
-Route::get('user/profile', 'UserController@profile');
-Route::post('user/update', 'UserController@update');
-Route::get('user/submissions', 'UserController@submissions');
-Route::get('user/sales', 'UserController@sales');
+
+Route::group(['middleware' => ['https']], function() {
+    Route::post('auth', 'AuthController@index');
+    Route::get('login', 'AuthController@login');
+    Route::get('signup', 'AuthController@signup');
+    Route::get('oauth/{service}', 'AuthController@oauth');
+    Route::get('oauth/callback/{service}', 'AuthController@oauthCallback');
+    Route::post('user/create', 'UserController@create');
+    Route::get('user/activate/{user}/{token}', 'UserController@activate');
+});
+
+Route::group(['middleware' => ['auth', 'https']], function() {
+    Route::get('logout', 'AuthController@logout');
+    Route::get('user', 'UserController@index');
+    Route::get('user/profile', 'UserController@profile');
+    Route::post('user/update', 'UserController@update');
+    Route::get('user/submissions', 'UserController@submissions');
+    Route::get('user/sales', 'UserController@sales');
+});
+
