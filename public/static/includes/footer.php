d8a4010b5ced640df071c32ec57de5ce61eb6dbc
Karen Banzon <kmbanzon@gmail.com>
2016-09-23 22:58:05 +0800

    Updating footer

--- public/static/includes/footer.php
+++ public/static/includes/footer.php
@@ -1,11 +1,21 @@
-				<footer class="text-center padding-20">
-					Copyright &copy; 2016, Teedlee Arts Inc.
-				</footer>
-
-                <script src="//code.jquery.com/jquery-1.11.2.min.js"></script>
-				<script src="js/ui.js"></script>
+                
 			</div>
+            <footer class="row expanded padding-20">
+                <div class="small-4 columns text-left">
+                    <a href="">About</a>
+                    <a href="">Shop</a>
+                </div>
+                <div class="small-4 columns text-center">
+                    <a href=""><span class="icon icon-facebook"></span></a>
+                    <a href=""><span class="icon icon-twitter"></span></a>
+                    <a href=""><span class="icon icon-instagram"></span></a>
+                </div>
+                <div class="small-4 columns text-right">
+                    &copy; 2016, Teedlee Arts Inc.
+                </div>
+            </footer>
 		</div>
-
+    <script src="//code.jquery.com/jquery-1.11.2.min.js"></script>
+    <script src="js/ui.js"></script>
 	</body>
 </html>
