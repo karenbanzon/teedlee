bcc726d2774c6767fbaebb249d2a7e7dec9e257f
Jho Estrella <jhourlad01@gmail.com>
2016-10-06 09:11:13 +0800

    favicon

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -1,12 +1,15 @@
 <!DOCTYPE html>
 <html>
 <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <title>Teedlee</title>
     {!! Html::style('//fonts.googleapis.com/css?family=Montserrat|Open+Sans') !!}
     {!! Html::script('bower_components/jquery/dist/jquery.min.js') !!}
     @yield('head')
     {!! Html::style('css/style.css') !!}
     {!! Html::style('css/app.min.css') !!}
+    <link rel="shortcut icon" href="{!! url('favicon.png') !!}" type="image/png" />
 </head>
 <body>
     <div id="site-wrapper">
