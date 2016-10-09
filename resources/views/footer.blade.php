ccf172ec5101cd9c6844f12a50da3369b5a492ee
Jho Estrella <jhourlad01@gmail.com>
2016-10-09 11:52:03 +0800

    nofollow attribute on external links

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,12 +1,12 @@
 <footer class="row expanded padding-20 mt20 clr">
     <div class="small-4 columns text-left">
         <a href="{!! url('about') !!}">About</a>
-        <a href="http://shop.teedlee.ph">Shop</a>
+        <a href="http://shop.teedlee.ph" rel="nofollow">Shop</a>
     </div>
     <div class="small-4 columns text-center">
-        <a href="{!! env('FB_URL') !!}"><span class="icon icon-facebook"></span></a>
-        <a href="{!! env('TWITTER_URL') !!}"><span class="icon icon-twitter"></span></a>
-        <a href="{!! env('INSTAGRAM_URL') !!}"><span class="icon icon-instagram"></span></a>
+        <a href="{!! env('FB_URL') !!}" rel="nofollow"><span class="icon icon-facebook"></span></a>
+        <a href="{!! env('TWITTER_URL') !!}" rel="nofollow"><span class="icon icon-twitter"></span></a>
+        <a href="{!! env('INSTAGRAM_URL') !!}" rel="nofollow"><span class="icon icon-instagram"></span></a>
     </div>
     <div class="small-4 columns text-right">
         &copy; 2016, Teedlee Arts Inc.
--- resources/views/nav.blade.php
+++ resources/views/nav.blade.php
@@ -2,13 +2,13 @@
     <h4 class="padding-20">Menu</h4>
     <hr>
     <nav>
-        <a href="http://shop.teedlee.ph">Shop</a>
+        <a href="http://shop.teedlee.ph" rel="nofollow">Shop</a>
         <a href="{!! url('submit') !!}">Submit</a>
         <a href="{!! url('votes') !!}">Vote</a>
     </nav>
     <hr>
     <nav class="secondary">
-        <a href="{!! url('cart') !!}"><span class="icon icon-shopping-cart"></span> Cart</a>
+        <a href="http://shop.teedlee.ph/cart" rel="nofollow"><span class="icon icon-shopping-cart"></span> Cart</a>
         @if( \Auth::check() )
             <a href="{!! url('user') !!}"><span class="icon icon-user"></span> My Account</a>
             <a href="{!! url('logout') !!}"><span class="icon icon-log-out"></span> Logout</a>
@@ -22,8 +22,8 @@
     <section class="row">
         <nav class="header text-right ">
             <a href="{!! url('') !!}" class="brand"><img src="{!! url('images/logo.png') !!}"></a>
-            <a href="http://shop.teedlee.ph/search" class="show-for-large"><span class="icon icon-search"></span></a>
-            <a href="http://shop.teedlee.ph" class="show-for-large">Shop</a>
+            <a href="http://shop.teedlee.ph/search" class="show-for-large" rel="nofollow"><span class="icon icon-search"></span></a>
+            <a href="http://shop.teedlee.ph" class="show-for-large" rel="nofollow">Shop</a>
             <a href="{!! url('submit') !!}" class="show-for-large">Submit</a>
             <a href="{!! url('votes') !!}" class="show-for-large">Vote</a>
 
@@ -40,7 +40,7 @@
                 <a href="{!! url('login') !!}" class="show-for-large">Login</a>
             @endif
 
-            <a href="http://shop.teedlee.ph/cart" class="show-for-large"><span class="icon icon-shopping-cart"></span></a>
+            <a href="http://shop.teedlee.ph/cart" class="show-for-large" rel="nofollow"><span class="icon icon-shopping-cart"></span></a>
             <a href="" class="toggle-nav hide-for-large"><span class="icon icon-menu"></span></a>
         </nav>
     </section>
