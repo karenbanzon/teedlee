df91ceebdde9b07161d51ad26b6cd31cb7e8d4d3
gpsea <gpsea@SEA-EO02AP.local>
2016-10-09 08:35:14 +0800

    Refinement on submit landing page

--- public/static/template-submit-landing.php
+++ public/static/template-submit-landing.php
@@ -3,6 +3,23 @@
 
     <section class="row">
         <div class="small-12 large-8 large-offset-2 text-center">
+            <hr>
+                <h2>Submit</h2>
+            <hr>
+            <div class="text-left">
+                <p class="text-center"><img src="images/turn-ideas-into-cash.png"></p>
+                <p>1. Download Our Submission Templates</p>
+                <p>Sign Up and download our submission templates to get started. You can choose the style and color of the apparel you want to design on.</p>
+                <p class="text-center"><a href="https://shop.teedlee.ph/pages/submission-templates" class="button tiny white">Download submission templates</a></p>
+                <p>2. Submit Your Design</p>
+                <p>We’re excited to see your idea! Lay out your design together with a short narrative and send it to us! Learn the other info you need to know.</p>
+                <p class="text-center"><a href="https://shop.teedlee.ph/pages/guidelines" class="button tiny white">Read our guidelines</a></p>
+                <p>3. Promote Your Work &amp; Earn</p>
+                <p>If your design makes it to the online store, promote your work to friends and family. You get to earn a profit from their purchases while they get to wear some cool stuff from you.</p>
+                <p class="text-center"><a href="https://shop.teedlee.ph/pages/pricing-profits-and-receiving-your-earnings" class="button tiny white">Find out how much you can earn</a></p>
+                <p class="text-center"><img src="images/mr-pencil.png"></p>
+            </div>
+            <hr>
             <h4>Open Submission</h4>
             <p><a href=""><img src="images/got-an-idea.jpg"></a></p>
             <a href="" class="button white small">Submit here!</a>
