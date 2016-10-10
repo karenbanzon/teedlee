10c06fd26dce522718544c12825f96e70e9cfe7e
Jho Estrella <jhourlad01@gmail.com>
2016-10-10 10:13:39 +0800

    Also allow administrators to override declined submissions on public voting

--- resources/views/admin/submission/partials/edit/public-voting-fail.blade.php
+++ resources/views/admin/submission/partials/edit/public-voting-fail.blade.php
@@ -1,5 +1,7 @@
 <div class="alert alert-danger">
     <p>Public voting has failed for this item and was rejected:
         {!! $submission->votes->public->average !!} starred, {!! $flags !!} flagged.</p>
-    <div class="clr"></div>
+    <p>
+        <a href="{!! url('admin/submission/promote/'.$submission->id.'/publication') !!}" class="btn btn-sm btn-warning">Overrride &amp; publish</a>
+    </p>
 </div>
