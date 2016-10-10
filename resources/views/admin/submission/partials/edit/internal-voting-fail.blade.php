eceafad5a790412649dda22d093a64bba058d90e
Jho Estrella <jhourlad01@gmail.com>
2016-10-10 10:12:37 +0800

    Allow administrators to override declined submissions

--- resources/views/admin/submission/partials/edit/internal-voting-fail.blade.php
+++ resources/views/admin/submission/partials/edit/internal-voting-fail.blade.php
@@ -1,7 +1,9 @@
 <div class="alert alert-danger">
     <p>Internal voting has failed for this item and was rejected:
         {!! $submission->votes->internal->average !!} starred, {!! $flags !!} flagged.</p>
-    <div class="clr"></div>
+    <p>
+        <a href="{!! url('admin/submission/promote/'.$submission->id.'/publication') !!}" class="btn btn-sm btn-warning">Overrride &amp; publish</a>
+    </p>
 </div>
 
 @include('admin.submission.partials.edit.internal-votes-box')
