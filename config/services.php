44a0b1f7310977d4759f8b2ba22feea638709bee
Jho Estrella <jhourlad01@gmail.com>
2016-10-10 12:44:59 +0800

    Configuration fixes

--- config/services.php
+++ config/services.php
@@ -39,7 +39,7 @@ return [
     'facebook' => [
         'client_id' => '260663264328654', //'173048843120426',
         'client_secret' => '559e06bd6ae6cb48a9dd2de3295fcc19', //'c28f658ff2fca09d79b8c1712eb60021',
-        'redirect' => secure_url('/oauth/callback/facebook'),
+        'redirect' => 'https://teedlee.ph/oauth/callback/facebook',
     ],
 
     'shopify' => [
