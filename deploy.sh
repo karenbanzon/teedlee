5b0828a680292dc056b8ad1486b96cd32c143d42
Jho Estrella <jhourlad01@gmail.com>
2016-10-28 09:57:42 +0800

    Create branch if not exist

--- deploy.sh
+++ deploy.sh
@@ -3,8 +3,7 @@
 branch=${1:-master}
 cd /var/www/html
 git fetch --all
-git checkout -b $branch
-if ! git checkout $branch;
+if ! git checkout -B $branch;
     then exit
 fi
 git pull origin $branch -f
