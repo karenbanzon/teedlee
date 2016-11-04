1efaf244b4fb157e2c0b2b40301435ec72a187b1
Karen Banzon <kmbanzon@gmail.com>
2016-11-04 14:39:54 +0800

    Added templates for sales tracking

---
+++ public/static/template-sales-overview.php
@@ -0,0 +1,69 @@
+
+<?php include 'includes/header.php' ?>
+
+    <section class="row">
+        <div class="small-12 large-8 large-offset-2">
+            <hr>
+            <h4 class="text-center">My Account</h4>
+            <hr>
+        </div>
+        <div class="small-12">
+            <div class="card small-2 show-for-large">
+                <nav class="account-nav">
+                    <a href="">Profile</a>
+                    <a href="" class="active">Submissions</a>
+                    <a href="">Sales</a>
+                </nav>
+            </div>
+            <div class="card small-12 hide-for-large">
+                <nav class="account-nav-small">
+                    <a href="">Profile</a>
+                    <a href="">Submissions</a>
+                    <a href="" class="active">Sales</a>
+                </nav>
+            </div>
+            <div class="card small-12 large-10 padding-20">
+                <div class="card small-12">
+                    <h6>Designs in store</h6>
+                    <table style="width: 100%;">
+                        <thead>
+                            <tr>
+                                <th>Design</th>
+                                <th>Title</th>
+                                <th>SRP</th>
+                                <th>Sold</th>
+                                <th>Earnings</th>
+                                <th>Actions</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>
+                                    <img src="http://placehold.it/160x120">
+                                </td>
+                                <td>Submission #1</td>
+                                <td>PHP 499.00</td>
+                                <td>18</td>
+                                <td>PHP 8,982.00</td>
+                                <td>
+                                    <a href="" class="button tiny white">View</a>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>&nbsp;</td>
+                                <td>&nbsp;</td>
+                                <td><h6>Total</h6></td>
+                                <td>18</td>
+                                <td>PHP 8,982.00</td>
+                                <td>
+                                    <a href="" class="button tiny">Remit</a>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </section>
+
+<?php include 'includes/footer.php' ?>
