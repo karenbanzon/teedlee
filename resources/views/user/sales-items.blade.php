b803aa121cb861112bb0505a049bf82c2709c21c
Jho Estrella <jhourlad01@gmail.com>
2016-11-14 22:51:12 +0800

    Fixes sidebar links on user/sales

--- resources/views/user/sales-items.blade.php
+++ resources/views/user/sales-items.blade.php
@@ -1,20 +1,7 @@
 @extends('master')
 @section('content')
     <div class="small-12">
-        <div class="card small-2 show-for-large">
-            <nav class="account-nav">
-                <a href="">Profile</a>
-                <a href="" class="active">Submissions</a>
-                <a href="">Sales</a>
-            </nav>
-        </div>
-        <div class="card small-12 hide-for-large">
-            <nav class="account-nav-small">
-                <a href="">Profile</a>
-                <a href="">Submissions</a>
-                <a href="" class="active">Sales</a>
-            </nav>
-        </div>
+        @include('user/sidebar')
         <div class="card small-12 large-10 padding-20">
             <div class="card small-12">
                 <a href="{!! url('user/sales') !!}" class="display-inline-block margin-top-10"><span class="icon icon-left"></span> Back to overview</a>
--- resources/views/user/sales.blade.php
+++ resources/views/user/sales.blade.php
@@ -8,20 +8,7 @@
                     <hr>
                 </div>
                 <div class="small-12">
-                    <div class="card small-2 show-for-large">
-                        <nav class="account-nav">
-                            <a href="">Profile</a>
-                            <a href="" class="active">Submissions</a>
-                            <a href="">Sales</a>
-                        </nav>
-                    </div>
-                    <div class="card small-12 hide-for-large">
-                        <nav class="account-nav-small">
-                            <a href="">Profile</a>
-                            <a href="">Submissions</a>
-                            <a href="" class="active">Sales</a>
-                        </nav>
-                    </div>
+                    @include('user/sidebar')
                     <div class="card small-12 large-10 padding-20">
                         <div class="card small-12">
                             <h6>Designs in store</h6>
