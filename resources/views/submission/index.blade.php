63430d31ecbc81f9267eecb3702b7e2bdc3fa028
Jho Estrella <jhourlad01@gmail.com>
2016-11-30 15:22:05 +0800

    Insert extra line in between items on submission landing page

--- resources/views/submission/index.blade.php
+++ resources/views/submission/index.blade.php
@@ -57,9 +57,13 @@
             <h4>1. Download Our Submission Templates</h4>
             <p>Sign Up and download our submission templates to get started. You can choose the style and color of the apparel you want to design on.</p>
             <p class="text-center"><a href="https://shop.teedlee.ph/pages/submission-templates" class="button white">Download submission templates</a></p>
+            <p>&nbsp;</p>
+
             <h4>2. Submit Your Design</h4>
             <p>We’re excited to see your idea! Lay out your design together with a short narrative and send it to us! Learn the other info you need to know.</p>
             <p class="text-center"><a href="https://shop.teedlee.ph/pages/guidelines" class="button white">Read our guidelines</a></p>
+            <p>&nbsp;</p>
+
             <h4>3. Promote Your Work &amp; Earn</h4>
             <p>If your design makes it to the online store, promote your work to friends and family. You get to earn a profit from their purchases while they get to wear some cool stuff from you.</p>
             <p class="text-center"><a href="https://shop.teedlee.ph/pages/pricing-profits-and-receiving-your-earnings" class="button white">Find out how much you can earn</a></p>
