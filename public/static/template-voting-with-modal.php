c491ff39b1d861ce042409465741f3584af4f0e2
Karen Banzon <kmbanzon@gmail.com>
2016-12-02 15:17:52 +0800

    Added template for vote page with sign up prompt modal

---
+++ public/static/template-voting-with-modal.php
@@ -0,0 +1,65 @@
+
+<?php include 'includes/header.php' ?>
+
+    <section class="row">
+        <div class="small-12 large-8 large-offset-2">
+            <hr>
+            <h4 class="text-center">VOTE</h4>
+            <hr>
+        </div>
+        <div class="small-12 columns">
+            <div class="card small-12 large-8 large-offset-2">
+                <div class="card-container">
+                    <div>
+                        <img src="http://placehold.it/400x300" alt="">
+                    </div>
+                    <div class="card-body item">
+                        <div class="item-name text-center"><h6>Bacon &amp; Eggs</h6></div>
+                        <div class="text-center">By <a href=""><strong>chaks</strong></a></div>
+                        <br>
+                        <div class="vote-actions text-center">
+                            <a href="" class="star star-1"><span class="icon icon-star"></span></a>
+                            <a href="" class="star star-2"><span class="icon icon-star"></span></a>
+                            <a href="" class="star star-3"><span class="icon icon-star"></span></a>
+                            <a href="" class="star star-4"><span class="icon icon-star"></span></a>
+                            <a href="" class="star star-5"><span class="icon icon-star"></span></a>
+                        </div>
+                        <div class="vote-comments">
+                            <form class="padding-20">
+                                <div class="form-field block">
+                                    <label>Comments</label>
+                                    <textarea rows="2" placeholder="What do you think of this design?"></textarea>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="card small-12 large-8 large-offset-2 text-center">
+                <a href="" class="button" id="showSignUp">Sign up to vote</a> <a href="" class="button white">View next</a>
+            </div>
+        </div>
+    </section>
+
+    <div class="modal" id="voteSignUp">
+        <div class="card">
+            <div class="card-container padding-bottom-20">
+                <div>
+                    <a href="" class="modal-close" id="closeSignUp"><span class="icon icon-x"></span></a>
+                    <h4 class="text-center">Sign up to vote!</h4>
+                </div>
+                <hr>
+                <div class="text-center padding-top-20">
+                    <a href=""><img src="https://z-1-scontent.fmnl3-2.fna.fbcdn.net/t39.2178-6/851579_209602122530903_1060396115_n.png"></a>
+                </div>
+                <div class="text-center padding-20">
+                    <a href="">Sign up with my email</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script>
+    </script>
+
+<?php include 'includes/footer.php' ?>
