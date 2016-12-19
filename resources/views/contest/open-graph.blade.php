0200119f99135aaeeb0fe081518ffa3cc4b9b5cc
Jho Estrella <jhourlad01@gmail.com>
2016-12-19 19:12:49 +0800

    Make contest description open graph description

--- resources/views/contest/open-graph.blade.php
+++ resources/views/contest/open-graph.blade.php
@@ -1,7 +1,7 @@
 <meta property="fb:app_id" content="{!! config('services.facebook.client_id') !!}">
 <meta property="og:type" content="product">
 <meta property="og:title" content="Teedlee - {!! $contest->title !!}">
-<meta property="og:description" content="Teedlee is a clothing brand powered by artists. We produce and sell crowd-sourced graphic designs from our community, where the artist of this design will earn a profit from your purchases.">
+<meta property="og:description" content="{!! $contest->description !!}">
 <meta property="og:url" content="{!! url('contest/'.$contest->id) !!}">
 <meta property="og:image" content="{!! url('contests/'.$contest->id.'/'.$contest->banner) !!}">
 {{--<meta property="product:price:amount" content="">--}}
