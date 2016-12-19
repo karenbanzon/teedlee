12e4e2bca48c60c95447b7bf352190c31f3ec455
Jho Estrella <jhourlad01@gmail.com>
2016-12-19 18:59:25 +0800

    Add 'Teedlee - ' to facebook graph title

--- resources/views/contest/open-graph.blade.php
+++ resources/views/contest/open-graph.blade.php
@@ -1,6 +1,6 @@
 <meta property="fb:app_id" content="{!! config('services.facebook.client_id') !!}">
 <meta property="og:type" content="product">
-<meta property="og:title" content="{!! $contest->title !!}">
+<meta property="og:title" content="Teedlee - {!! $contest->title !!}">
 <meta property="og:description" content="Teedlee is a clothing brand powered by artists. We produce and sell crowd-sourced graphic designs from our community, where the artist of this design will earn a profit from your purchases.">
 <meta property="og:url" content="{!! url('contest/'.$contest->id) !!}">
 <meta property="og:image" content="{!! url('contests/'.$contest->id.'/'.$contest->banner) !!}">
